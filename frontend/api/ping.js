5875ed3b4878ca6997d52159b92500a040035b3a
Krenil Donga <krenildonga@gmail.com>
2026-04-21 16:24:10 +0530

    fix vercel 5

--- frontend/api/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({ 
-    success: true, 
-    message: "pong from frontend standalone ping", 
-    timestamp: new Date().toISOString() 
-  });
-};
